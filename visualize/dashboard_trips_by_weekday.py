d4ede32ca0b72f069fc67fecd82c38fb3b2fc4f9
hannah-ross <hannahross33@gmail.com>
2018-07-12 11:25:51 -0700

    more viz options for viewing trips by weekday

--- visualize/dashboard_trips_by_weekday.py
+++ visualize/dashboard_trips_by_weekday.py
@@ -1,6 +1,9 @@
 
 '''
     This file will generate an html file to serve as a precursor for a dashboard file.
+    Current visuals: trips per day of week (pie chart, bar plot, grouped bar plot)
+            
+                    
     Name and Password fields will need to be changed for 'connect'.
     
     Author: Hannah Ross
@@ -35,11 +38,7 @@ def get_data(con):
                                        index_col=None)
     return (trips_db,status_change_db)
 
-# INFORMATION GOES HERE
-user = "username"
-password = "password"
-db = "database"
-con = connect(user,password,db)
+con = connect("hannah1ross","password","transit")
 tdb, scdb = get_data(con)
 
 def obs_in_month(month,pd_df):
@@ -84,35 +83,96 @@ def plot_trips_per_weekdays_for_interval(firstday,lastday,tdb ):
     the_interval = calendar.month_name[firstday.month] +' ' +str(firstday.day)+ ' to '+ calendar.month_name[lastday.month] +' ' +str(lastday.day)
     #import plotly.plotly as py
 
-    fig = {
+    pie_fig = {
       "data": [
         {
           "values": [mon_count,tues_count,wed_count,thurs_count,fri_count,sat_count,sun_count ],
           "labels": [x for x in calendar.day_name],
-          "name": "Company Ridership",
-          "hoverinfo":"label+name+value",
+          #"name": "Company Ridership",
+          "hoverinfo":"label+value",
           "type": "pie"
         },
         ],
       "layout": {
-            "title":"Trips per Weekday for {}".format(the_interval),
+            "title":"Trips per Day of Week from {}".format(the_interval),
         }
     }
-    return fig
+    bar_fig = {
+        "data": [
+        {
+          "y": [mon_count,tues_count,wed_count,thurs_count,fri_count,sat_count,sun_count ],
+          "x": [x for x in calendar.day_name],
+          #"name": "Company Ridership",
+          "hoverinfo":"value",
+          "type": "bar"
+        },
+        ],
+      "layout": {
+            "title":"Trips per Day of Week from {}".format(the_interval),
+            "yaxis":{"title":"Number of Trips"}
+        }     
+    }
+    #trips_df = obs_in_days(firstday ,lastday , tdb)
+#the_interval = calendar.month_name[firstday.month] +' ' +str(firstday.day)+ ' to '+ calendar.month_name[lastday.month] +' ' +str(lastday.day)
+
+    bat_trips_df = trips_df.loc[trips_df['company_name']=='Bat']
+    lemon_trips_df = trips_df.loc[trips_df['company_name']=='Lemon']
+
+    bat_trips_by_day = get_days_of_trips(bat_trips_df)
+    lemon_trips_by_day = get_days_of_trips(lemon_trips_df)
+
+    bat_mon_count,lemon_mon_count = count_days('Monday',bat_trips_by_day),count_days('Monday',lemon_trips_by_day),
+    bat_tues_count,lemon_tues_count = count_days('Tuesday',bat_trips_by_day), count_days('Tuesday',lemon_trips_by_day)
+    bat_wed_count,lemon_wed_count = count_days('Wednesday',bat_trips_by_day),count_days('Wednesday',lemon_trips_by_day)
+    bat_thurs_count,lemon_thurs_count = count_days('Thursday',bat_trips_by_day),count_days('Thursday',lemon_trips_by_day)
+    bat_fri_count,lemon_fri_count = count_days('Friday',bat_trips_by_day),count_days('Friday',lemon_trips_by_day)
+    bat_sat_count, lemon_sat_count = count_days('Saturday',bat_trips_by_day), count_days('Saturday',lemon_trips_by_day)
+    bat_sun_count, lemon_sun_count = count_days('Sunday',bat_trips_by_day),count_days('Sunday',lemon_trips_by_day)
+
+    trace1 = go.Bar(
+        y=[bat_mon_count,bat_tues_count,bat_wed_count,bat_thurs_count,bat_fri_count,bat_sat_count,bat_sun_count ],
+        x= [x for x in calendar.day_name],
+        name='Bat'
+    )
+
+    trace2 = go.Bar(
+        y=[lemon_mon_count,lemon_tues_count,lemon_wed_count,lemon_thurs_count,lemon_fri_count,lemon_sat_count,lemon_sun_count ],
+        x= [x for x in calendar.day_name],
+        name='Lemon'
+    )
+
+    data=[trace1,trace2]
+    layout = go.Layout(
+        barmode='group',
+        title="Trips per Day of Week from {}".format(the_interval),
+        yaxis={"title":"Number of Trips"}
+    )
+
+    double_bar_fig = go.Figure(data=data, layout=layout)
+    #py.iplot(double_bar_fig, filename='grouped-bar_trips')
+
+    
+    return pie_fig,bar_fig,double_bar_fig
 
     #py.iplot(fig, filename='trips_per_day')
     
-result_fig = plot_trips_per_weekdays_for_interval(datetime.datetime(2018, 8, 3, 8, 32, 13) ,datetime.datetime(2018, 8, 7, 8, 33, 13),tdb )
+pie_fig,bar_fig,double_bar_fig = plot_trips_per_weekdays_for_interval(datetime.datetime(2018, 8, 3, 8, 32, 13) ,datetime.datetime(2018, 8, 10, 8, 33, 13),tdb )
 #py.iplot(result_fig,filename='trips_per_day') # this generates plot inline
 
-first_plot_url = py.plot(result_fig, filename='trips_per_weekday', auto_open=False,)
+pie_plot_url = py.plot(pie_fig, filename='trips_per_weekdayPie', auto_open=False,)
+bar_plot_url = py.plot(bar_fig, filename='trips_per_weekdayBar', auto_open=False,)
+double_plot_url = py.plot(double_bar_fig, filename='trips_per_weekdayDoubleBar', auto_open=False,)
 html_string = '''
 <html>
         <!-- *** Section 1 *** --->
-        <h2>Viualizing the trips taken per day of week</h2>
+        
         <iframe width="1000" height="550" frameborder="0" seamless="seamless" scrolling="no" \
-src="''' + first_plot_url + '''.embed?width=800&height=550"></iframe>
-        <p>More trips were takaen on Saturday and Sunday .</p>
+src="''' + pie_plot_url + '''.embed?width=800&height=550"></iframe>
+        
+        <iframe width="1000" height="550" frameborder="0" seamless="seamless" scrolling="no" \
+src="''' + bar_plot_url + '''.embed?width=800&height=550"></iframe>
+        <iframe width="1000" height="550" frameborder="0" seamless="seamless" scrolling="no" \
+src="''' + double_plot_url + '''.embed?width=800&height=550"></iframe>
         
     </body>
 </html>'''
