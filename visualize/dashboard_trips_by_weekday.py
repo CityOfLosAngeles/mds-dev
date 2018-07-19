f86c0e4283fc3c1c4730d237006fdd7f91167cf2
hannah-ross <hannahross33@gmail.com>
2018-07-19 16:10:50 -0700

    added breakdown of reasons for availability

--- visualize/dashboard_trips_by_weekday.py
+++ visualize/dashboard_trips_by_weekday.py
@@ -1,30 +1,28 @@
 '''
-    This notebook will generate an html file to serve as a precursor for a dashboard file.
-    Current visuals: trips per day of week (pie chart, bar plot, grouped bar plot, time of trips plot)
-    ** modified for new dabase structure
+This script generates an html file 'dash_testing.html' of dashboard visualizations for the trips and status_change data.
     
-    Name and Password fields will need to be changed for 'connect'.
+Name and Password fields will need to be changed for 'connect' to read in data from the server.
     
-    Author: Hannah Ross
+Author: Hannah Ross
 '''
 
 import calendar
 import datetime
 import plotly.plotly as py
+import plotly.graph_objs as go
 import sqlalchemy
-from mapbox import Datasets
 import os
-from mapboxgl.utils import create_color_stops
-from mapboxgl.viz import CircleViz
-import psycopg2
-import pandas
 import json
 import requests
 import ast
-import pandas as pd
+import pandas
 from mapboxgl.utils import *
 from mapboxgl.viz import *
-import plotly.graph_objs as go
+import fiona
+import shapely.wkt
+import shapely.geometry
+import sqlalchemy
+import pyproj
 
 def connect(user,password,db,host='localhost',port=5432):
     url = 'postgresql://{}:{}@{}:{}/{}'
@@ -38,48 +36,44 @@ def get_data(con):
                                        index_col=None)
     return (trips_db,status_change_db)
 
-# read in trips and status change data from the server
+# read in trips and status_change data from the server
 con = connect("hannah1ross","password","transit")
 tdb, scdb = get_data(con)
 
-# returns observations in a given month's time frame
+# returns trip observations in a given month's time frame
 def obs_in_month(month,pd_df):
     start_time = [pd_df['route'][i]['features'][0]['properties']['timestamp'] for i in range(len(pd_df))]
     mo = month
-    vec=[datetime.datetime.utcfromtimestamp(x) for x in start_time[0:len(pd_df)] if datetime.datetime.utcfromtimestamp(x).month==mo]
-    bool_vec = [d.month==mo for d in start_time]
+    bool_vec = [datetime.datetime.utcfromtimestamp(d).month==mo for d in start_time]
     return pd_df.loc[bool_vec].reset_index()
 
-# return observations in a given hour's time frame - not day specific
+# return trip observations in a given hour's time frame - not day specific
 def obs_in_hour(hour,pd_df):
     start_time = [pd_df['route'][i]['features'][0]['properties']['timestamp'] for i in range(len(pd_df))]
     hr = hour
-    vec=[datetime.datetime.utcfromtimestamp(x) for x in start_time[0:len(pd_df)] if datetime.datetime.utcfromtimestamp(x).hour==hr]
-    bool_vec = [d.hour==hr for d in start_time]
+    bool_vec = [datetime.datetime.utcfromtimestamp(d).hour==hr for d in start_time]
     return pd_df.loc[bool_vec].reset_index()
 
-# return observations within a period of 2 specified days (first and last days must be unix time stamps: datetime.strptime('2015-10-20 22:24:46', '%Y-%m-%d %H:%M:%S') )
+# return trip observations within a period of 2 specified days  (days are datetime objects)
 def obs_in_days(firstday,lastday,pd_df):
     start_time = [pd_df['route'][i]['features'][0]['properties']['timestamp'] for i in range(len(pd_df))]
-    vec=[datetime.datetime.utcfromtimestamp(x) for x in start_time[0:len(pd_df)] if ((datetime.datetime.utcfromtimestamp(x)<= firstday) & (datetime.datetime.utcfromtimestamp(x)>=lastday))]
     bool_vec = [((datetime.datetime.utcfromtimestamp(d) >=firstday) & (datetime.datetime.utcfromtimestamp(d)<= lastday)) for d in start_time]
     return pd_df.loc[bool_vec].reset_index()
 
-# extracts the days of each trip
-def get_days_of_trips(tripsdf): # for plotting trips per day of week
+# extracts the days of each trip for plotting trips taken per day of week
+def get_days_of_trips(tripsdf):
     start_time = [tripsdf['route'][i]['features'][0]['properties']['timestamp'] for i in range(len(tripsdf))]
     return [calendar.day_name[datetime.datetime.utcfromtimestamp(x).weekday()] for x in start_time]
 
-# counts the frequency of each day given vector of days
-def count_days(day,dayvec): # for plotting trips per day of week
+# counts the frequency of each day given vector of days for plotting trips per day of week
+def count_days(day,dayvec):
     vec=[dayvec[i]==day for i in range(len(dayvec))]
     return sum(vec)
 
-# returns plots for trip taken per day of week
+# returns 3 plots for the number of trips taken per day of week (pie chart, bar plot, & double bar plot)
 def plot_trips_per_weekdays_for_interval(firstday,lastday,tdb ):
 
     trips_df = obs_in_days(firstday ,lastday , tdb)
-    #trips_df=trips_df.reset_index()
     trips_by_day = get_days_of_trips(trips_df)
     
     mon_count = count_days('Monday',trips_by_day)
@@ -96,7 +90,6 @@ def plot_trips_per_weekdays_for_interval(firstday,lastday,tdb ):
              {
              "values": [mon_count,tues_count,wed_count,thurs_count,fri_count,sat_count,sun_count ],
              "labels": [x for x in calendar.day_name],
-             #"hoverinfo":"label+value",
              "type": "pie"
              },
              ],
@@ -109,7 +102,6 @@ def plot_trips_per_weekdays_for_interval(firstday,lastday,tdb ):
              {
              "y": [mon_count,tues_count,wed_count,thurs_count,fri_count,sat_count,sun_count ],
              "x": [x for x in calendar.day_name],
-             #"hoverinfo":"value",
              "type": "bar"
              },
              ],
@@ -160,11 +152,15 @@ def plot_trips_per_weekdays_for_interval(firstday,lastday,tdb ):
 pie_fig,bar_fig,double_bar_fig = plot_trips_per_weekdays_for_interval(datetime.datetime(2018, 8, 3, 8, 32, 13) ,datetime.datetime(2018, 8, 10, 8, 33, 13),tdb )
 
 # get urls of images for trips per day
+print "Building plot: 1"
 pie_plot_url = py.plot(pie_fig, filename='trips_per_weekdayPie', auto_open=False,)
+print "Building plot: 2"
 bar_plot_url = py.plot(bar_fig, filename='trips_per_weekdayBar', auto_open=False,)
+print "Building plot: 3"
 double_plot_url = py.plot(double_bar_fig, filename='trips_per_weekdayDoubleBar', auto_open=False,)
 
-def to_twelve_hour(hour): # used for plotting trips per hour
+# used for plotting trips per hour
+def to_twelve_hour(hour):
     if hour > 12:
         new=hour-12
         return str(new)+'PM'
@@ -175,6 +171,7 @@ def to_twelve_hour(hour): # used for plotting trips per hour
     else:
         return str(hour)+'AM'
 
+# plot the number of trips taken per hour
 def plot_trips_per_hour(tdb):
     start_times = [tdb['route'][i]['features'][0]['properties']['timestamp'] for i in range(len(tdb))]
     hour_vec=[datetime.datetime.fromtimestamp(d).hour for d in start_times]
@@ -197,65 +194,163 @@ def plot_trips_per_hour(tdb):
     layout.xaxis=dict(title= "Hour of Day",tickmode='linear')
     layout.plot_bgcolor='rgb(11, 0, 0)'
     hours_fig=go.Figure(data=data,layout=layout)
-    return hours_fig
+    return py.plot(hours_fig, filename='trips_per_hour', auto_open=False,)
 
-# can filter tdb using obs_in functions for window specification
+# can filter tdb using obs_in functions for date window specification
 tdb_filtered = obs_in_days(datetime.datetime(2018, 8, 3, 8, 32, 13) ,datetime.datetime(2018, 8, 10, 8, 33, 13),tdb)
-hours_fig = plot_trips_per_hour(tdb_filtered)
-hours_plot_url = py.plot(hours_fig, filename='trips_per_hour', auto_open=False,)
+print "Building plot: 4"
+hours_plot_url = plot_trips_per_hour(tdb_filtered)
 
-# create map of start points of trips
-# create dataframes for startpoints and endpoints with lat and long coords
+# creates pie chart for each companies percent of total trips taken
+def plot_trips_per_company(pd_trips_df,firstday,lastday):
+    dat = obs_in_days(firstday, lastday, pd_trips_df)
+    the_interval = calendar.month_name[firstday.month] +' ' +str(firstday.day)+ ' to '+ calendar.month_name[lastday.month] +' ' +str(lastday.day)
+    
+    bat_users = sum(dat['company_name']=='Bat')
+    lemon_users = sum(dat['company_name']=='Lemon')
+    
+    fig = {
+        "data": [
+                 {
+                 "values": [],
+                 "labels": [
+                            "Bat",
+                            "Lemon"
+                            ],
+                 "hoverinfo":"label+value",
+                 "type": "pie"
+                 },
+                 ],
+            "layout": {
+            "title":"Trips Per Company from {}".format(the_interval),
+                    }
+                }
+    fig['data'][0]['values'].append(bat_users)
+    fig['data'][0]['values'].append(lemon_users)
+    return py.plot(fig, filename='TripsPerCompany',auto_open=False)
 
-'''
-start_d = {'lat':[], 'lon':[]}
-end_d = {'lat':[], 'lon':[]}
+print "Building plot: 5"
+company_plot_url = plot_trips_per_company(tdb,datetime.datetime(2018, 8, 3, 8, 32, 13),datetime.datetime(2018, 8, 10, 8, 33, 13))
 
-end_points = [tdb['route'][i]['features'][1]['geometry']['coordinates'] for i in range(len(tdb))]
-start_points = [tdb['route'][i]['features'][0]['geometry']['coordinates'] for i in range(len(tdb))]
+# takes in trips df, plots the number of trips taken in each council district
+# for trips, does not record status change service dropoff at start of day.
+# get each council district boundary'
 
-for start_p,end_p in zip(start_points,end_points):
-    start_lon,start_lat,end_lon, end_lat = start_p[0],start_p[1],end_p[0],end_p[1]
-    start_d['lat'].append(start_lat)
-    start_d['lon'].append(start_lon)
-    end_d['lat'].append(end_lat)
-    end_d['lon'].append(end_lon)
+bounds = fiona.open('/Users/hannah1ross/Desktop/mds-dev/data/CouncilDistricts.shp')# fix file path issue
 
-startdb = pandas.DataFrame.from_dict(start_d) # db is just lat and long points
-enddb = pandas.DataFrame.from_dict(end_d) # db is just lat and long points
-
-# Must be a public token, starting with `pk`
-token = os.getenv('MAPBOX_TOKEN')
-
-# Create a geojson file export from a Pandas dataframe
-df_to_geojson(startdb, filename='points.geojson',lat='lat', lon='lon')
-
-#Create a heatmap
-heatmap_color_stops = create_color_stops([0.01,0.25,0.5,0.75,1], colors='RdPu')
-heatmap_radius_stops = [[0,1], [15, 40]] #increase radius with zoom
-
-color_breaks = [0,10,100,1000,10000]
-color_stops = create_color_stops(color_breaks, colors='Spectral')
-
-heatmap_weight_stops = create_weight_stops(color_breaks)
-
-#Create a heatmap
-viz = HeatmapViz('points.geojson',
-                 access_token=token,
-                 weight_stops = heatmap_weight_stops,
-                 color_stops = heatmap_color_stops,
-                 radius_stops = heatmap_radius_stops,
-                 opacity = 0.9,
-                 center = (-118, 34),
-                 zoom = 3,
-                 )
-
-x=viz.show()
-
-'''
+all_bounds = []
+for i in range(12):
+    original = pyproj.Proj(bounds.crs)
+    dest = pyproj.Proj(init='epsg:4326')
+    polygons = []
+    polygons_list = []
+    for poly in bounds[i]['geometry']['coordinates']: # get district 10
+        polygon = [] # eventual converted polygon
+        polygon_lists = []
+        for x,y in poly:
+            x_prime,y_prime = pyproj.transform(original,dest,x,y) # transform point
+            p = (x_prime,y_prime)
+            polygon.append(p)
+            polygon_lists.append([x_prime,y_prime])
+    polygons.append(shapely.geometry.Polygon(polygon))
+    polygons_list.append(polygon_lists)
+    boundary = shapely.geometry.MultiPolygon(polygons) # o
+    all_bounds.append(boundary)
 
 
-# configure the html with all plots
+def plot_trips_per_cd(tdb):
+    bat_tdb = tdb.loc[tdb['company_name']=='Bat']
+    lemon_tdb = tdb.loc[tdb['company_name']=='Lemon']
+    lemon_tdb=lemon_tdb.reset_index()
+    bat_tdb = bat_tdb.reset_index()
+    bat_start_points = [bat_tdb['route'][i]['features'][0]['geometry']['coordinates'] for i in range(len(bat_tdb))]
+    lemon_start_points = [lemon_tdb['route'][i]['features'][0]['geometry']['coordinates'] for i in range(len(lemon_tdb))]
+    
+    lemon_dc_counts = []
+    bat_dc_counts = []
+    # count the number of trips beginning in each dc
+    for i in range(12):
+        boundary = all_bounds[i]
+        bat_dc_count = 0
+        lemon_dc_count = 0
+        for bat_pt,lemon_pt in zip(bat_start_points,lemon_start_points):
+            bat_pt = shapely.geometry.Point(bat_pt)
+            lemon_pt = shapely.geometry.Point(lemon_pt)
+            if boundary.contains(bat_pt):
+                bat_dc_count = bat_dc_count + 1
+            if boundary.contains(lemon_pt):
+                lemon_dc_count = lemon_dc_count + 1
+        lemon_dc_counts.append(lemon_dc_count)
+        bat_dc_counts.append(bat_dc_count)
+    
+    # plotting trips per council district (double bar per company)
+    trace1 = go.Bar(
+                    y=[count for count in bat_dc_counts ],
+                    x= [x+1 for x in range(12)],
+                    name='Bat'
+                    )
+
+    trace2 = go.Bar(
+                y=[count for count in lemon_dc_counts ],
+                x= [x+1 for x in range(12)],
+                name='Lemon'
+                )
+    
+    data=[trace1,trace2]
+    layout = go.Layout(
+                       barmode='group',
+                       title="Trips Beginning in Each Council District",
+                       yaxis={"title":"Number of Trips"},
+                       xaxis={"title":"Council District"},
+                       )
+        
+    trips_per_cd_fig = go.Figure(data=data, layout=layout)
+    return py.plot(trips_per_cd_fig,filename='trips_per_cd', auto_open=False,)
+
+print "Building plot: 6"
+trips_per_cd_plot_url = plot_trips_per_cd(tdb)
+
+
+# show chart of the most common reasons for device availability
+def plot_availability_piechart(scdb):
+    avail_rows = scdb.loc[scdb['event_type']=='available']
+    service_start = sum(avail_rows['reason']=='service_start')
+    maintenance = sum(avail_rows['reason']== 'maintenance')
+    maintenance_drop_off = sum(avail_rows['reason']== 'maintenance_drop_off')
+    user_drop_off = sum(avail_rows['reason']== 'user_drop_off')
+    user_pick_up = sum(avail_rows['reason']== 'user_pick_up')
+    low_battery = sum(avail_rows['reason']== 'low_battery')
+    service_end = sum(avail_rows['reason']== 'service_end')
+    rebalance_pick_up = sum(avail_rows['reason']== 'rebalance_pick_up')
+    maintenance_pick_up = sum(avail_rows['reason']== 'maintenance_pick_up')
+    out_of_service_area_pick_up = sum(avail_rows['reason']== 'out_of_service_area_pick_up')
+    out_of_service_area_drop_off = sum(avail_rows['reason']== 'out_of_service_area_drop_off')
+
+    fig = {
+    "data": [
+             {
+             "values": [],
+             "labels": ["service_start","maintenance","maintenance_drop_off","user_drop_off","user_pick_up","low_battery","service_end","rebalance_pick_up","maintenance_pick_up","out_of_service_area_pick_up","out_of_service_area_drop_off"],
+             "name": "Availability Breakdown",
+             "hoverinfo":"label+name+value",
+             "type": "pie"
+             },
+             ],
+        "layout": {
+        "title":"Categories of Availability"
+        }
+    }
+
+    lis = [service_start,maintenance,maintenance_drop_off,user_drop_off,user_pick_up,low_battery,service_end,rebalance_pick_up,maintenance_pick_up,out_of_service_area_pick_up,out_of_service_area_drop_off ]
+    for val in lis:
+        fig['data'][0]['values'].append(val)
+    return py.plot(fig, filename='AvailabilityPie', auto_open=False,)
+
+print "Building plot: 7"
+availability_pie_url = plot_availability_piechart(scdb)
+
+
+# configure the html with all plot urls
 html_string = '''
     <html>
     <!-- *** Section 1 *** --->
@@ -272,9 +367,18 @@ html_string = '''
         <iframe width="1000" height="550" frameborder="0" seamless="seamless" scrolling="no" \
         src="''' + hours_plot_url + '''.embed?width=900&height=550"></iframe>
             
+        <iframe width="1000" height="550" frameborder="0" seamless="seamless" scrolling="no" \
+        src="''' + company_plot_url + '''.embed?width=900&height=550"></iframe>
             
-        </body>
-    </html>'''
+        <iframe width="1000" height="550" frameborder="0" seamless="seamless" scrolling="yes" \
+            src="''' + trips_per_cd_plot_url + '''.embed?width=900&height=550"></iframe>
+                
+        <iframe width="1000" height="550" frameborder="0" seamless="seamless" scrolling="yes" \
+                src="''' + availability_pie_url + '''.embed?width=900&height=550"></iframe>
+        
+            
+            </html>
+ '''
 
 print('Generating \'dash_testing.html\' ...')
 f = open('dash_testing.html','w')
