0f36f9148c4048a5b4acdd04eb94acf5f3101d17
dmklinger <dmklinger@comcast.net>
2018-07-20 14:19:14 -0700

    Revert "Finished views. Added them to make_tables and reset."
    This reverts commit b98c4e42e91cd241792f2f4224f78fd00a04a889.

--- server/reset.sql
+++ server/reset.sql
@@ -2,5 +2,3 @@ DROP TABLE IF EXISTS trips;
 DROP TABLE IF EXISTS status_change;
 DROP TYPE IF EXISTS event_type;
 DROP TYPE IF EXISTS reason;
-DROP VIEW IF EXISTS availability;
-DROP VIEW IF EXISTS geometry;
