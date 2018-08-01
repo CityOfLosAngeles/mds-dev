5ccd94d1354b11daf846cb00ee8edea7de6706c5
New Hannah Ross <hannah1ross@Hannah-Rosss-MacBook-2.local>
2018-08-01 15:30:26 -0700

    accepts specified companies and credentials

---
+++ visualize/controls.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Required file to be imported for generate_dashboard.py
+
+File should be updated as needed to account for variables like the name of companies permited to operate, 
+plotly credential settings, and shapefile filepaths
+- company names
+- shapefiles
+
+Author : Hannah Ross
+"""
+# provide plotly account username and api_key
+SETS = {'username':'hannahross33','api_key':'8YSEgJklJCdTJv4u9UIv'}
+
+# provide list of companies permited to operate and included in database
+COMPANIES = ['Bat','Lemon']
+
+
